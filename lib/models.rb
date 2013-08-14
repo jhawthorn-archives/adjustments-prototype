976ccfef0272b7d523ca839fc7d13ae3bf3cd786
John Hawthorn <john@freerunningtechnologies.com>
2013-08-14 11:17:55 -0700

    Order#add_product
    Change-Id: Ia230db0a3cdfc137999285d9bfaa20a499035088

--- lib/models.rb
+++ lib/models.rb
@@ -1,20 +1,55 @@
 require 'money'
+require 'bigdecimal'
 
-class Product < Struct.new(:name, :price)
+class Product
+  attr_reader :name, :price
+  def initialize name, price
+    @name = name
+    @price = BigDecimal.new(price)
+  end
 end
 
-class Order < Struct.new(:items)
+class LineItem
+  attr_reader :cost, :quantity, :product, :currency
+  def initialize product, quantity, currency
+    @product, @quantity, @currency = product, quantity, currency
+    @cost = product.price
+  end
+  def money
+    Money.new(cost * quantity, currency)
+  end
+  def taxation
+    BigDecimal.new('0.14')
+  end
+end
+
+class Order
+  attr_reader :items
+  def initialize
+    @items = []
+  end
   def currency
     'USD'
   end
   def total
-    Money.new(0.0, currency)
+    item_total + tax_total
+  end
+  def item_total
+    items.inject(zero) do |m,item|
+      m + item.money
+    end
   end
   def tax_total
-    Money.new(0.0, currency)
+    items.inject(zero) do |m,item|
+      m + item.money * item.taxation
+    end
   end
-end
-
-class LineItem < Struct.new(:order, :cost, :quantity, :product)
+  def add_product product, quantity=1
+    items << LineItem.new(product, quantity, currency)
+  end
+  private
+  def zero
+    Money.new(0, currency)
+  end
 end
 
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,9 +1,24 @@
 require 'spec_helper'
 
 describe Order do
+  let(:order){ Order.new }
+  subject{ order }
   context 'a new order' do
-    subject{ Order.new }
     specify{ subject.total.should == Money.new(0.0, 'USD') }
     specify{ subject.tax_total.should == Money.new(0.0, 'USD') }
+  end
+  describe 'add_product' do
+    let(:product){ Product.new('Half Life 3', '39.99') }
+    before do
+      order.add_product product
+    end
+    specify{ order.items.count.should == 1 }
+    specify{ order.item_total.should == Money.new(39.99, 'USD') }
+    specify{ order.total.should == order.item_total + order.tax_total }
+    describe "added item" do
+      subject{ order.items.first }
+      specify{ subject.product.should == product }
+      specify{ subject.money.should == Money.new(39.99, 'USD') }
+    end
   end
 end
